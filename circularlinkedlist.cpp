886b48dc2f70db79c7ee23d1307e98362bb62a30
smriti <42511215+smriti7857@users.noreply.github.com>
2020-05-09 23:58:48 +0530

    Add files via upload

---
+++ circularlinkedlist.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+	public:
+		int key;
+		int data;
+		Node* next;
+		Node()
+		{
+			key=0;
+			data=0;
+			next=NULL;
+		}
+		Node(int k,int d)
+		{
+			key=k;
+			data=d;
+		}
+		
+};
+
+class CircularLinkedList
+{
+
+
+
+
